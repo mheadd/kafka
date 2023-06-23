13f0f6077e87cd750f0612fa30b3b569feb0f4fa
mheadd <mheadd@gmail.com>
2023-06-23 10:03:30 -0400

    Formatting

--- consumer.js
+++ consumer.js
@@ -6,23 +6,21 @@ const topic = process.argv[3] || config.defaultTopic;
 
 // Create a new KafkaJS instance.
 const kafka = new Kafka({
-	clientId: config.clientId,
-	brokers: [`${config.brokerIP}:${config.brokerPort}`]
+  clientId: config.clientId,
+  brokers: [`${config.brokerIP}:${config.brokerPort}`]
 });
 
 const getMessage = async function () {
-    const consumer = kafka.consumer({ groupId: config.groupId });
-
-    await consumer.connect();
-    await consumer.subscribe({ topic: topic, fromBeginning: true });
-    
-    await consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
-        console.log({
-          value: message.value.toString(),
-        })
-      },
-    });
+  const consumer = kafka.consumer({ groupId: config.groupId });
+  await consumer.connect();
+  await consumer.subscribe({ topic: topic, fromBeginning: true });
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      })
+    },
+  });
 }
 
 getMessage();
